751d5abd5058174c0ff4a2180a8fc23cfaa943fe
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-08 18:23:32 +1000

    creating custom instance

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { ToastContainer } from "react-toastify";
+import { nanoid } from "nanoid";
+import Form from "./Form";
+import Items from "./Items";
+import { useState } from "react";
+const defaultItems = [
+  { id: nanoid(), title: "walk the dog", isDone: false },
+  { id: nanoid(), title: "wash dishes", isDone: false },
+  { id: nanoid(), title: "drink coffee", isDone: true },
+  { id: nanoid(), title: "take a nap", isDone: false },
+];
+const App = () => {
+  const [items, setItems] = useState(defaultItems);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form />
+      <Items items={items} />
+    </section>
+  );
+};
+export default App;

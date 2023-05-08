41e60df273821d9c7f0d5052d0a87d4abb6ed7c0
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-08 22:29:28 +1000

    rendering data

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,15 +1,22 @@
 import SingleItem from "./SingleItem";
 import customFetch from "./utils";
 import { useQuery } from "@tanstack/react-query";
-const Items = ({ items }) => {
-  const result = useQuery({
+const Items = ({}) => {
+  const { isLoading, data } = useQuery({
     queryKey: ["tasks"],
-    queryFn: () => customFetch.get(""),
+    queryFn: async () => {
+      const { data } = await customFetch.get("");
+      return data;
+    },
   });
-  console.log(result);
+  // console.log(result);
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading ..</p>;
+  }
+  // console.log(data);
   return (
     <div className="items">
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>

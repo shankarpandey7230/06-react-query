ca0f9120ab35746c44583e0b129e0cbc10c15f68
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-09 14:14:02 +1000

    handling error

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,11 +1,11 @@
 import SingleItem from "./SingleItem";
 import customFetch from "./utils";
 import { useQuery } from "@tanstack/react-query";
-const Items = ({}) => {
-  const { isLoading, data } = useQuery({
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
     queryKey: ["tasks"],
     queryFn: async () => {
-      const { data } = await customFetch.get("");
+      const { data } = await customFetch.get("/something");
       return data;
     },
   });
@@ -14,6 +14,14 @@ const Items = ({}) => {
     return <p style={{ marginTop: "1rem" }}>Loading ..</p>;
   }
   // console.log(data);
+  if (isError) {
+    return <p style={{ marginTop: "1rem" }}>There was an error</p>;
+  }
+  // console.log(error);
+  // if (error) {
+  //   return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
+  // }
+
   return (
     <div className="items">
       {data.taskList.map((item) => {
